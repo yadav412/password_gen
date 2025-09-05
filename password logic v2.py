ad2493aa0049a1ba61faac8387ebba194da730ca
yadav412 <yadavsingh3012@gmail.com>
2025-09-05 09:20:39 -0700

    feat: added encrypt /decrypt algo

--- password logic v2.py
+++ password logic v2.py
@@ -9,7 +9,7 @@ sp= ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "=", "+", "|", ":", ";", 
 
 def generate_password(length):
     password=[]
-    password=""
+    password1=""
     for i in range(length):
         if i%4== 0:
             password.append(secret.choice(sp)) # adding in a mandatory special character
@@ -19,9 +19,9 @@ def generate_password(length):
     random.shuffle(password) # shuffling this should be rewriten to be more secure then using and exsisting library
 
     for i in range(len(password)):
-        password=password+ str(password[i])
+        password1=password1+ str(password[i])
       
-    return password
+    return password1
 
 
 def generate_pin(length):
@@ -42,7 +42,43 @@ def generate_pin(length):
 
 #coding logic rewrite complexity
 
+#basic encryption logic // ceaser(modded) cipher shift 
 
+#common_words =["the", "be", "to", "of", "and", "a", "in", "that", "have", "I", "dog", "cat", "man", "woman", "child", "car", "house", "tree", "food", "water"]
+Plain = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+Cipher = ["X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W"]
+
+#encrypt function (old ceaser cipher)
+#chaining multiple encryptions together to make it more complex - the encryption 
+
+#the encryption engine will be made more complex in future iterations "Chimera Encryption Engine"
+
+
+def encryptceaser(message):
+    
+    em=""
+    for i in range(len(message)):
+        for j in range(len(Plain)):
+            if message[i] == Plain[j]:
+                em= em + Cipher[j]
+    return em
+
+def Dencryptceaser(message): #works are ceaser cipher ecryption (happy accident)
+    
+    em=""
+    for i in range(len(message)):
+        for j in range(len(Plain)):
+            if message[i] == Cipher[j]:
+                em= em + Plain[j]
+    return em
+
+
+
+print(encryptceaser("HELLO"))
+
+print(Dencryptceaser("HELLO"))
+
+print(Dencryptceaser("EBIIL")) #the decryption logic works basic decryption and encryption is functional
 
 
 
